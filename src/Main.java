8705aee8148c060cfbd702604e9979462026546b
ForSunFlower <923341856@qq.com>
2019-11-01 11:25:56 +0800

    测试上传代码

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("测试成功了好像");
+        HashMap<String, Object> stringObjectHashMap = new HashMap<>();
+        stringObjectHashMap.put("a", "hehehe");
+        stringObjectHashMap.put("b", "hahaha");
+        stringObjectHashMap.put("c", "xixixi");
+        System.out.println(stringObjectHashMap);
+        System.out.println("hhhh");
     }
 }
