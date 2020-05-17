73782fe7f6f6e3e0c60a4ca8ed2f42abe0b2b57b
liuchenyang <923341856@qq.com>
2020-05-17 14:43:01 +0800

    double类型比较

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
-import java.util.HashMap;
+import java.text.ParseException;
 
 public class Main {
-
-    public static void main(String[] args) {
-        HashMap<String, Object> stringObjectHashMap = new HashMap<>();
-        stringObjectHashMap.put("a", "hehehe");
-        stringObjectHashMap.put("b", "hahaha");
-        stringObjectHashMap.put("c", "xixixi");
-        System.out.println(stringObjectHashMap);
-        System.out.println("测试提交名称");
-        System.out.println("hhhh");
+    public static void main(String[] args) throws ParseException {
+        double a = -30.6;
+        double b = -30.6;
+        if(a==b){
+            System.out.println("xd");
+        } else {
+            System.out.println("retretre");
+        }
     }
 }
