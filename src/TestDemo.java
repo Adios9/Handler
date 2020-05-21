307c71dd52e984dba74e6e2ac81228f1a6b82fc5
liuchenyang <923341856@qq.com>
2020-05-21 16:26:39 +0800

    final

---
+++ src/TestDemo.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: Adiós.
+ * @Date: 2020/5/21 16:25
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        final int a = 10;
+        System.out.println(a);
+    }
+}
