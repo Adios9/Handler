58875fd251411af688a5abd3f4496a0a00d2429c
liuchenyang <923341856@qq.com>
2020-05-30 15:23:16 +0800

    final

--- src/TestDemo.java
+++ src/TestDemo.java
@@ -1,9 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @Author: Adiós.
  * @Date: 2020/5/21 16:25
  */
 public class TestDemo {
     public static void main(String[] args) {
-        final int a = 10;
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        list1.add(5);
+
+        List<String> stringList = list1.stream().map(String::valueOf).collect(Collectors.toList());
+        for (int i=0; i<stringList.size(); i++){
+            System.out.println(stringList.get(i));
+        }
+
+        ArrayList<String> list2 = new ArrayList<>();
+        /*list2.add("2");
+        list2.add("3");
+        list2.add("4");
+        list2.add("5");
+        list2.add("6");*/
+
+        list2.removeAll(list1);
     }
 }
